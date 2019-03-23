69f2b8f3a0b1e2470d3d4da5f7a2ef705717d66a
David Bettis <david@davidbettis.com>
2019-03-23 11:02:39 -0700

    add copyright notice for apocrphya verses (NRSV)

--- website/src/pages/about.js
+++ website/src/pages/about.js
@@ -27,7 +27,9 @@ const AboutPage = () => (
 
     <h3>Scripture</h3>
 
-    <p>Unless otherwise indicated, all Scripture quotations are from the ESV® Bible (The Holy Bible, English Standard Version®), copyright © 2001 by Crossway, a publishing ministry of Good News Publishers. Used by permission. All rights reserved. You may not copy or download more than 500 consecutive verses of the ESV Bible or more than one half of any book of the ESV Bible.</p>
+    <p>Scripture quotations from the Old Testament and the New Testament are from the ESV® Bible (The Holy Bible, English Standard Version®), copyright © 2001 by Crossway, a publishing ministry of Good News Publishers. Used by permission. All rights reserved. You may not copy or download more than 500 consecutive verses of the ESV Bible or more than one half of any book of the ESV Bible.</p>
+
+    <p>Scripture quotations from the Apocrypha are from the New Revised Standard Version Bible, copyright © 1989 National Council of the Churches of Christ in the United States of America. Used by permission. All rights reserved worldwide.</p>
 
   </Layout>
 )
