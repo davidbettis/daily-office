4d0b3b1179e7bd7617661fb06e5a47e9461386a2
davidbettis <david@davidbettis.com>
2020-03-24 21:23:20 -0700

    make psalms more structured with an array of psalm references

--- website/src/components/sections/psalter.js
+++ website/src/components/sections/psalter.js
@@ -13,22 +13,22 @@ function Psalter(props) {
     var month = props.date.getMonth() + 1; // month, 1-12
     var day = props.date.getDate(); // day, 1-31
 
-    var psalm;
+    var psalms;
     if (props.series === 'morning') {
-        psalm = MorningPsalmLectionary[month][day];
+        psalms = MorningPsalmLectionary[month][day];
     } else if (props.series === 'evening') {
-        psalm = EveningPsalmLectionary[month][day];
+        psalms = EveningPsalmLectionary[month][day];
     } else {
         throw new Error("Psalter error: series must specify one of ['morning','evening']");
     }
 
-    var psalmRef = "Psalm+" + psalm;
+    var psalmRef = "Psalm+" + psalms.join(',');
     var psalmLinkText = "(ESV)";
 
     return (
         <div>
           <p className="section">Psalms Appointed</p>
-          <p>Psalm { psalm } <ESVLink scriptureText={psalmRef} linkText={psalmLinkText} /></p>
+          <p>Psalm { psalms.join(', ') } <ESVLink scriptureText={psalmRef} linkText={psalmLinkText} /></p>
           <p>
             <span className="officiant">☩ Glory be to the Father, and to the Son, and to the Holy Spirit; *</span><br/>
             <span className="people">As it was in the beginning, is now, and ever shall be, world without end. Amen.</span><br/>
