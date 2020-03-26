f27d0534f26e92a0fa3e56defa06ca95025ba526
davidbettis <david@davidbettis.com>
2020-03-26 16:25:33 -0700

    Cache calls to scripture-service in-memory to eliminate duplicate requests, one for psalms & another for lessons.

--- website/src/helpers/scripture-service.js
+++ website/src/helpers/scripture-service.js
@@ -1,16 +1,44 @@
 // Interface to interact with the web service that returns text from the ESV.
 //
 // TODO cache the results of these service calls on the client
+
+
 export class ScriptureService {
+
+    static SCRIPTURE_SERVICE_CACHE = {}
+    static SCRIPTURE_SERVICE_ENDPOINT = 'https://pd0vgs56hb.execute-api.us-east-1.amazonaws.com/default/';
 
     // Gets the lessons and psalms for the provided office/date combo.
     // @return a promise with the results of the service call
     static getScriptureForOffice(office, date) {
-        return fetch(ScriptureService.scriptureServiceEndpoint(ScriptureService.ymd(date), office), {
+
+        var dateYmd = ScriptureService.ymd(date);
+
+        // Check the in-memory cache and return a successful response, if present
+        var cacheKey = office + '#' + dateYmd;
+        var cacheEntry = ScriptureService.SCRIPTURE_SERVICE_CACHE[cacheKey];
+        if (cacheEntry) {
+            return Promise.resolve(new Response(new Blob([cacheEntry])));
+        }
+
+        // Otherwise, fetch the contents
+        var webServiceFullEndpoint = ScriptureService.SCRIPTURE_SERVICE_ENDPOINT + 'daily-office-get-scripture?'
+                    + 'date=' + dateYmd + '&office=' + office;
+
+        var promise = fetch(webServiceFullEndpoint, {
             method: "GET",
             // TODO eliminate this
             credentials: "same-origin"
         });
+
+        // ... and cache it, on success
+        promise.then(result => {
+            ScriptureService.SCRIPTURE_SERVICE_CACHE[key] = result;
+            return result;
+        });
+
+        // Finally, return the original request for final processing by the client.
+        return promise;
     }
 
     // Returns the Y-M-D version of the provided date (Date object).
@@ -20,13 +48,6 @@ export class ScriptureService {
         var day = date.getDate(); // day, 1-31
 
         return year + '-' + String(month).padStart(2, '0') + '-' + String(day).padStart(2, '0');
-    }
-
-    static scriptureServiceEndpoint(date, office) {
-        // TODO make this configurable
-        return 'https://pd0vgs56hb.execute-api.us-east-1.amazonaws.com'
-            + '/default/daily-office-get-scripture?'
-            + 'date=' + date + '&office=' + office;
     }
 }
 
